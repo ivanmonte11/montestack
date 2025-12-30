17af0b70b97c8ff072a2376edad737ad8793de64
ivan monte <ivanmonte07.93@gmail.com>
2025-12-30 13:00:16 -0300

    Correxion db.ts

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -4,6 +4,9 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db = globalForPrisma.prisma ?? new PrismaClient();
+// Exportamos una función o el objeto, pero asegurando que no explote en el build
+export const db = globalForPrisma.prisma ?? new PrismaClient({
+  log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
